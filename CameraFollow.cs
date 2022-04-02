b1a54a4392906ed73ebeff580765b6f21c07fdfe
ZXC TESLA <87894848+TeslaDevQ@users.noreply.github.com>
2022-04-02 13:03:56 +0300

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Vector2 DefaultResolution = new Vector2(720, 1280);
+    [Range(0f, 1f)] public float WidthOrHeight = 0;
+
+    private Camera componentCamera;
+    
+    private float initialSize;
+    private float targetAspect;
+
+    private float initialFov;
+    private float horizontalFov = 120f;
+    
+    public Transform Player;
+    public float dampTime = 0.2f;
+    private Vector3 cameraPos;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        componentCamera = GetComponent<Camera>();
+        initialSize = componentCamera.orthographicSize;
+
+        targetAspect = DefaultResolution.x / DefaultResolution.y;
+
+        initialFov = componentCamera.fieldOfView;
+        horizontalFov = CalcVerticalFov(initialFov, 1 / targetAspect);
+    }
+
+    private void Update()
+    {
+        if (componentCamera.orthographic)
+        {
+            float constantWidthSize = initialSize * (targetAspect / componentCamera.aspect);
+            componentCamera.orthographicSize = Mathf.Lerp(constantWidthSize, initialSize, WidthOrHeight);
+        }
+        else
+        {
+            float constantWidthFov = CalcVerticalFov(horizontalFov, componentCamera.aspect);
+            componentCamera.fieldOfView = Mathf.Lerp(constantWidthFov, initialFov, WidthOrHeight);
+        }
+        cameraPos = new Vector3(0.52f, Player.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPos, ref velocity, dampTime);
+    }
+
+    private float CalcVerticalFov(float hFovInDeg, float aspectRatio)
+    {
+        float hFovInRads = hFovInDeg * Mathf.Deg2Rad;
+
+        float vFovInRads = 2 * Mathf.Atan(Mathf.Tan(hFovInRads / 2) / aspectRatio);
+
+        return vFovInRads * Mathf.Rad2Deg;
+    }
+}
